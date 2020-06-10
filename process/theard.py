128c0b0cb242dbb4961afa53c96d1a0e7e7e4b51
shitingbao <871258317@qq.com>
2020-06-10 17:46:03 +0800

    thread

--- process/theard.py
+++ process/theard.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+import threading
+
+
+def out_name(name):
+    for i in range(5):
+        print(name, ":", i)
+
+
+def thread_test():
+    tone = threading.Thread(target=out_name("123"), name="123")
+    tone.start()
+    ttwo = threading.Thread(target=out_name("456"), name="456")
+    ttwo.start()
