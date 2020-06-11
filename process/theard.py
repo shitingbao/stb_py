e27dcd10bcba6ef88f9b538058a741f908f5d8cb
shitingbao <871258317@qq.com>
2020-06-11 13:50:11 +0800

    thread

--- process/theard.py
+++ process/theard.py
@@ -8,7 +8,14 @@ def out_name(name):
 
 
 def thread_test():
-    tone = threading.Thread(target=out_name("123"), name="123")
-    tone.start()
-    ttwo = threading.Thread(target=out_name("456"), name="456")
+    # 这里要用元组，不然会将这个字符串的每一个字符都当成一个参数，引起参数个数不匹配的问题
+    tone = threading.Thread(target=out_name, args=("123",))
+    print("this first")
+    ttwo = threading.Thread(target=out_name, args=("456",))
+
     ttwo.start()
+    tone.start()
+
+
+if __name__ == "__main__":
+    thread_test()
